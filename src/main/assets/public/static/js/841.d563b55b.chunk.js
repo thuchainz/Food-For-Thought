6843033f5ca80b996cee7d250af978bcadf09326
thuchainz <74222399+thuchainz@users.noreply.github.com>
2023-08-05 09:35:50 -0700

    Add files via upload

---
+++ src/main/assets/public/static/js/841.d563b55b.chunk.js
@@ -0,0 +1,3 @@
+/*! For license information please see 841.d563b55b.chunk.js.LICENSE.txt */
+"use strict";(self.webpackChunkfood_for_thought=self.webpackChunkfood_for_thought||[]).push([[841],{841:(t,e,o)=>{o.r(e),o.d(e,{startStatusTap:()=>a});var n=o(554),r=o(344),s=o(811);const a=()=>{const t=window;t.addEventListener("statusTap",(()=>{(0,n.wj)((()=>{const e=t.innerWidth,o=t.innerHeight,a=document.elementFromPoint(e/2,o/2);if(!a)return;const i=(0,r.a)(a);i&&new Promise((t=>(0,s.c)(i,t))).then((()=>{(0,n.Iu)((async()=>{i.style.setProperty("--overflow","hidden"),await(0,r.s)(i,300),i.style.removeProperty("--overflow")}))}))}))}))}}}]);
+//# sourceMappingURL=841.d563b55b.chunk.js.map
